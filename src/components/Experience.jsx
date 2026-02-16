ca6c9b456bd7c7848e7a80f17e6eddd203716b39
S-Muthuselvakumar <muthuselvakumar1911@gmail.com>
2026-02-16 20:56:42 +0530

    fix: experience correction

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,17 +4,17 @@ function Experience() {
       <h2 className="text-3xl font-semibold mb-8">Experience</h2>
 
       <div className="bg-gray-50 p-6 rounded-lg shadow-sm">
-        <h3 className="text-xl font-semibold">Software Developer - Product-Based Startup</h3>
+        <h3 className="text-xl font-semibold">Software Developer - Packapill Technologies Pvt Ltd</h3>
 
         <p className="text-gray-500 mb-3">
-          Packapill Technologies Pvt Ltd | 2024 August - Present
+          2024 August - Present
         </p>
 
         <ul className="list-disc  ml-5 space-y-2 text-gray-700">
-          <li>• Built and maintained healthcare booking flows using React.</li>
-          <li>• Integrated Stripe payment system for secure transactions.</li>
-          <li>• Integrated and optimized RESTful APIs for real-time appointment booking and patient data management.</li>
-          <li>• Collaborated with UI and backend teams in agile environment.</li>
+          <li>Built and maintained healthcare booking flows using React.</li>
+          <li>Integrated Stripe payment system for secure transactions.</li>
+          <li>Integrated and optimized RESTful APIs for real-time appointment booking and patient data management.</li>
+          <li>Collaborated with UI and backend teams in agile environment.</li>
         </ul>
       </div>
     </section>
